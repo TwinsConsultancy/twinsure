2265ece2f9ec601f631d480a31f479c20fb81412
nareshdharmaraj <nareshd2006@gmail.com>
2026-05-17 13:15:25 +0530

    updated the ui of the index, about, contact pages, fixed a allignment bug in admin page contacts tab

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,1 @@
-const API_BASE_URL = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' 
-    ? 'http://127.0.0.1:8000' 
-    : '/backend/api';
+const API_BASE_URL = 'http://127.0.0.1:8000'; 
